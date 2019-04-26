d2fab29788c189bfc26b4663c982d13cf0c3d1bd
wz5mz <wz5mz@virginia.edu>
2019-04-25 23:46:47 -0700

    Update enter_size.py

--- enter_size.py
+++ enter_size.py
@@ -4,13 +4,14 @@ from oauth2client.service_account import ServiceAccountCredentials
 import gspread
 import numpy as np
 import sys
+import re
 
 filename = sys.argv[1]
 sheetname = sys.argv[2]
-mouseno = sys.argv[3]
-width = float(sys.argv[4])
-length = float(sys.argv[5])
-date = sys.argv[6]
+mouseno = re.search('\d+', sys.argv[3]).group(0)
+width = float(re.search('(\d*\.\d+|\d+)', sys.argv[4]).group(0))
+length = float(re.search('(\d*\.\d+|\d+)', sys.argv[5]).group(0))
+date = re.search('\d\d\d\d-[0-1]\d-[0-3]\d', sys.argv[6]).group(0)
 
 
 def enter_size(filename, sheetname, mouseno, width, length, date):
@@ -22,33 +23,34 @@ def enter_size(filename, sheetname, mouseno, width, length, date):
              'https://www.googleapis.com/auth/drive']
     credentials = ServiceAccountCredentials.from_json_keyfile_name(
         '/data/data/com.termux/files/home/storage/downloads/Tumor Entry-f14983e25cb2.json', scope)
+#    credentials = ServiceAccountCredentials.from_json_keyfile_name(
+#        'c:/users/wade/downloads/Tumor Entry-f14983e25cb2.json', scope)
     gc = gspread.authorize(credentials)
     worksheet = gc.open(filename).worksheet(sheetname)
 
-    # find cell to update
+    # create list of cells to update [gspread.models.Cell]
+    cell_list = []
+    row_headers = np.array(worksheet.col_values(1))
+    if np.where(row_headers == mouseno)[0].size == 1:
+        myRow = np.where(row_headers == mouseno)[0][0] + 1
+    else:
+        print('Error. Either mouse ' + mouseno + ' does not exist or there are duplicate entries')
     column_headers = np.array(worksheet.row_values(1))
     if np.where(column_headers == date)[0].size > 0:
         myCol = np.where(column_headers == date)[0][0]
     else:
         myCol = len(column_headers)
-        worksheet.update_cell(1, myCol+1, date)
-        worksheet.update_cell(1, myCol+2, date)
-        worksheet.update_cell(1, myCol+3, date)
-    row_headers = np.array(worksheet.col_values(1))
-    if np.where(row_headers == mouseno)[0].size == 1:
-        myRow = np.where(row_headers == mouseno)[0] + 1
-    else:
-        print('Error. Either mouse ' + mouseno + ' does not exist or there are duplicate entries')
+        cell_list += [gspread.models.Cell(1, myCol+1, str(date)),
+                      gspread.models.Cell(1, myCol+2, str(date)),
+                      gspread.models.Cell(1, myCol+3, str(date))]
+    cell_list += [gspread.models.Cell(myRow, myCol+1, width),
+                  gspread.models.Cell(myRow, myCol+2, length),
+                  gspread.models.Cell(myRow, myCol+3,
+                                      float(width)*float(length))]
 
-    # update
-    worksheet.update_cell(myRow, myCol+1, width)
-    worksheet.update_cell(myRow, myCol+2, length)
-    worksheet.update_cell(myRow, myCol+3, float(width)*float(length))
-
-    # # update v2
-    # cell_list = worksheet.range('A1:B2')
-    # print(cell_list)
-    # worksheet.update_cells(cell_list)
+    # update - faster if only 1 call is made
+    print(cell_list)
+    worksheet.update_cells(cell_list)
 
 
 enter_size(filename, sheetname, mouseno, width, length, date)
