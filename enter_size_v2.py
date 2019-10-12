a7cf74cccf11329e996eafa6ce812eb7ebb08bce
wz5mz <wz5mz@virginia.edu>
2019-10-12 13:45:25 -0700

    Update enter_size_v2.py

--- enter_size_v2.py
+++ enter_size_v2.py
@@ -1,9 +1,61 @@
 #! /data/data/com.termux/files/usr/bin/env python3
+
+from oauth2client.service_account import ServiceAccountCredentials
+import pygsheets
+import numpy as np
 import sys
+import re
 import time
 
-userinput = sys.argv[3]
-print(userinput)
-print(type(userinput))
-print(len(userinput))
+filename = sys.argv[1]
+sheetname = sys.argv[2]
+mouseno = re.search('\d+', sys.argv[3]).group(0)
+measurements = [i for i in sys.argv[4] if i!=',']
+#width = float(re.search('(\d*\.\d+|\d+)', sys.argv[4]).group(0))
+#length = float(re.search('(\d*\.\d+|\d+)', sys.argv[5]).group(0))
+date = re.search('\d\d\d\d-[0-1]\d-[0-3]\d', sys.argv[5]).group(0)
+
+# def enter_size_v2(filename, sheetname, mouseno, measurements, date):
+#     # authenticate and read google spreadsheet
+#     scope = ['https://spreadsheets.google.com/feeds',
+#              'https://www.googleapis.com/auth/drive']
+# #    gc = pygsheets.authorize(service_file='/data/data/com.termux/files/home/storage/downloads/Tumor Entry-f14983e25cb2.json')
+#     gc = pygsheets.authorize(service_file='c:/users/wade/downloads/Tumor Entry-f14983e25cb2.json')
+#     sheet = gc.open(filename)
+#     worksheet = sheet.worksheet_by_title(sheetname)
+#     cell_list = []
+#     # find the row to enter user input
+#     row_headers = np.array([i[0] for i in worksheet.get_values(start=(1,1), end=(worksheet.rows,1), returnas='matrix')])
+#     myRow = int(np.where(row_headers == mouseno)[0][0] + 1)
+#     print(myRow)
+#     # find the col to enter user input
+#     column_headers = worksheet.get_values(start=(1,1), end=(1,worksheet.cols), returnas='matrix')[0]
+#     while column_headers[-1] == '':
+#         column_headers.pop()
+#     column_headers = np.array(column_headers)
+#     if np.where(column_headers == date)[0].size > 0:
+#         myCol = int(np.where(column_headers == date)[0][0])
+#     else:
+#         myCol = int(len(column_headers))
+#         cell_list += [pygsheets.Cell((1, myCol+1), str(date)),
+#                       pygsheets.Cell((1, myCol+2), str(date)),
+#                       pygsheets.Cell((1, myCol+3), str(date))]
+#
+#     area_cell = pygsheets.Cell((myRow, myCol+3))
+#     area_cell.formula = str(float(width))+'*'+str(float(length))
+#     cell_list += [pygsheets.Cell((myRow, myCol+1), width),
+#                   pygsheets.Cell((myRow, myCol+2), length),
+#                   area_cell]
+#     print(myCol)
+#     print(cell_list)
+#
+# #    worksheet.cell((myRow, myCol+1)).formula = str(width)+'*'+str(length)+'+'+''+str(5)+'*'+str(6)
+#
+#     if not worksheet.cell((myRow, myCol+1)).value and not worksheet.cell((myRow, myCol+2)).value and not worksheet.cell((myRow, myCol+3)).value:
+#         worksheet.update_cells(cell_list)
+#
+# if len(measurements) == 4 or len(measurements) == 2:
+#     enter_size_v2(filename, sheetname, mouseno, measurements, date)
+
+print(measurements)
 time.sleep(5)
