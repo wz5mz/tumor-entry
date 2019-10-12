04bb6ff37e440b1e17aa119f310477f3719e8d22
wz5mz <wz5mz@virginia.edu>
2019-10-12 14:51:32 -0700

    Update enter_size_v2.py

--- enter_size_v2.py
+++ enter_size_v2.py
@@ -10,7 +10,7 @@ import time
 filename = sys.argv[1]
 sheetname = sys.argv[2]
 mouseno = re.search('\d+', sys.argv[3]).group(0)
-print(sys.argv[])
+print(sys.argv[4])
 print(sys.argv[4].split(','))
 measurements = [re.search('(\d*\.\d+|\d+)', i).group(0) for i in sys.argv[4].split(',')]
 # date = re.search('\d\d\d\d-[0-1]\d-[0-3]\d', sys.argv[5]).group(0)
